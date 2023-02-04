611d3a76a49ea1f16e8bc92bb12a9846b50ac762
Tobiloba David Igiebor <77983084+omini25@users.noreply.github.com>
2023-02-04 14:32:56 +0100

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-//import AuthNavigation from 'components/lib/forms/auth-navigation';
+import AuthNavigation from 'components/lib/forms/auth-navigation';
 import BusinessLoginForm from 'components/lib/login/login-form';
 import AuthIcon from 'components/lib/forms/auth-icon';
 import Seo from 'lib/seo';
